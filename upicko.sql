af69433db82604ce3cbb6f32974257c7368cb847
Frank Tao <crispcake@gmail.com>
2017-09-11 22:58:16 +1000

    New detail page design (complete)

--- upicko.sql
+++ upicko.sql
@@ -6,3 +6,7 @@ alter table listings add column upicko_access_fee varchar(255);
 alter table listings add column upicko_unit_fee varchar(255);
 alter table listings add column upicko_facility_info text;
 alter table listings add column upicko_brief_info text;
+
+update listings set upicko_season_status = 1;
+alter table listings MODIFY upicko_season_status INT NOT NULL DEFAULT 1;
+
