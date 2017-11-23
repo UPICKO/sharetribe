b097c757c23e4f6791f37be986b1578651b53984
franktao1 <franktao1@nbnco.com.au>
2017-11-23 13:44:21 +1100

    Fix Bug: Booking button appears in PYO farm detail page!

--- app/assets/javascripts/upicko_detailPageJs.js
+++ app/assets/javascripts/upicko_detailPageJs.js
@@ -2,21 +2,27 @@ $(function() {
     if (window.location.href.indexOf("/listings/") > 0) {
         // In view list item detail page
         if(window.location.href.indexOf("edit") <= 0 && window.location.href.indexOf("new") <= 0) {
-            //Load the farmer's list in listing detail page
-            if (window.location.href.indexOf("edit") <= 0 && window.location.href.indexOf("new") <= 0 && $("#listing-author-link").length > 0 && $("#listing-author-link").attr('href') && $("#listing-author-link").text() != 'Upicko') {
-                $.ajax({
-                    type: "GET",
-                    url: $("#listing-author-link").attr('href'),
-                    success: function (resultData) {
-                        var profileListingsList = $('#profile-listings-list', $(resultData));
-                        var profileListLabel = profileListingsList.prev();
-                        var listingDetailContainer = $('.page-content .wrapper');
-                        listingDetailContainer.append($(profileListLabel.html() + profileListingsList.html()));
-                    },
-                    error: function (xhr, ajaxOptions, thrownError) {
-                        console.log(xhr);
-                    }
-                });
+            if (window.location.href.indexOf("edit") <= 0 && window.location.href.indexOf("new") <= 0 && $("#listing-author-link").length > 0 && $("#listing-author-link").attr('href')) {
+                if($("#listing-author-link").text() != 'Upicko') {
+                    //Load the farmer's list in listing detail page
+                    $.ajax({
+                        type: "GET",
+                        url: $("#listing-author-link").attr('href'),
+                        success: function (resultData) {
+                            var profileListingsList = $('#profile-listings-list', $(resultData));
+                            var profileListLabel = profileListingsList.prev();
+                            var listingDetailContainer = $('.page-content .wrapper');
+                            listingDetailContainer.append($(profileListLabel.html() + profileListingsList.html()));
+                        },
+                        error: function (xhr, ajaxOptions, thrownError) {
+                            console.log(xhr);
+                        }
+                    });
+                }
+
+                if($.trim($('#listing-message-links #booking-dates .enabled-book-button .content').html()) !== 'Upick Booking') {
+                    $('#listing-message-links').show();
+                }
             }
         }
     }
